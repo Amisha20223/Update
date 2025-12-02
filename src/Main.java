9a533d5b0591cd1c741dd615a8bcf967f3996d84
Amisha Singh <amishasingh@example.com>
2025-12-02 11:14:04 +0530

    student

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Student s = new Student(101, "Amisha", 85, 90, 80);
+        s.displayReport();
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,43 @@
+
+class Student {
+    public int studentId;
+    public String name;
+    public int[] marks = new int[3];
+
+    public Student(int studentId, String name, int m1, int m2, int m3) {
+        this.studentId = studentId;
+        this.name = name;
+        marks[0] = m1;
+        marks[1] = m2;
+        marks[2] = m3;
+    }
+
+    public int calculateTotal() {
+        return marks[0] + marks[1] + marks[2];
+    }
+
+    public double calculateAverage() {
+        return calculateTotal() / 3.0;
+    }
+
+    public String calculateGrade() {
+        double avg = calculateAverage();
+
+        if (avg >= 90) return "A";
+        else if (avg >= 75) return "B";
+        else if (avg >= 60) return "C";
+        else if (avg >= 40) return "D";
+        else return "F";
+    }
+
+    public void displayReport() {
+        System.out.println("----- Student Report -----");
+        System.out.println("Student ID: " + studentId);
+        System.out.println("Name: " + name);
+        System.out.println("Marks: " + marks[0] + ", " + marks[1] + ", " + marks[2]);
+        System.out.println("Total: " + calculateTotal());
+        System.out.println("Average: " + calculateAverage());
+        System.out.println("Grade: " + calculateGrade());
+        System.out.println("--------------------------");
+    }
+}
