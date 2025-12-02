d1f4e4fcfbe9d2e486825b7c1f59f0b42038f48b
Amisha Singh <120022430+Amisha20223@users.noreply.github.com>
2025-12-02 13:06:37 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Student s = new Student(101, "Amisha", 85, 90, 80);
+        Student s = new Student(177, "Amisha", 85, 90, 80);
         s.displayReport();
     }
 }
